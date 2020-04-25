595afa749d1bbdb7b40eaac963efc6f9ad249bf7
Рустем Фархутдинов <Rastveider@gmail.com>
2020-04-26 00:48:54 +0300

    Задание  18

---
+++ addressbook_web_test/addressbook_test_autoit/addressbook_test_autoit/test/GroupCreationTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace addressbook_test_autoit
+{
+    [TestFixture]
+    public class GroupCreationTest : TestBase
+    {
+        [Test]
+        public void TestGroupCreation()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            GroupData newGroup = new GroupData()
+            {
+                Name = "NewTest"
+            };
+
+            app.Groups.Add(newGroup);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(newGroup);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+
+        [Test]
+        public void TestGroupRemove()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            app.Groups.Delete(0);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.RemoveAt(0);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+
+
+    }
+}
